49fd91f705d68a75dd50d7ae5ecce65b6aedc40b
Jorge Martinez <jorgemswork@gmail.com>
2020-11-16 22:15:14 +0100

    Vemos los numero dentro de la imagen

--- personal_portfolio_project/blog/views.py
+++ personal_portfolio_project/blog/views.py
@@ -4,4 +4,6 @@ from .models import Blog
 def all_blogs(request):
     Blogs=Blog.objects.order_by('-date')
     return render(request,'blog/all_blogs.html', {'Blogs':Blogs})
+def detail(request,blog_id):
+    return render(request,'blog/detail.html', {'id':blog_id})
 
